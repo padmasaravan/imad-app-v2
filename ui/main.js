d93eb8e48fc866e0957e752295aa065b643fcbc3
padmasaravan <padma.saravan@gmail.com>
2017-02-16 00:37:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,6 @@
 console.log('Loaded!');
+
+// Code to change the HTML content from Javascript
+
+var element = document.getElementById('body-text');
+element.innerHTML = 'New Text changed by JavaScript';
