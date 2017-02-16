a5edf1f1cd1b29978614c23b8bc917756c072ca2
padmasaravan <padma.saravan@gmail.com>
2017-02-16 12:54:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ var marginLeft =0;
 img.onclick = function(){
     var interval = setInterval(moveRight,50);  // call the moveRight funtion every 50ms
     function moveRight(){
-        if(marginLeft <=750){
+        if(marginLeft <=50){
             marginLeft = marginLeft + 5;
             img.style.marginLeft = marginLeft + 'px';
         }else{
