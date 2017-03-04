0a025224b1fbe3da0c212d0eb12a9226cfd54c4f
padmasaravan <padma.saravan@gmail.com>
2017-03-05 01:19:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config = {
     database: 'padmasaravan',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: 'db-padmasaravan-62727'
+    password: process.env.DB_PASSWORD
 };
 
 var app = express();
