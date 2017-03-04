d26481a10fcb6dbd7ff6b73ea0f0d4c0962dc87b
padmasaravan <padma.saravan@gmail.com>
2017-03-04 23:38:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+//Database Confiquration Details
+var config = {
+    user: 'padmasaravan',
+    database: 'padmasaravan',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
