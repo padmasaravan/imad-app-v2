0b258fe6662fdb9e9d790ad5b08bbecafda58c0c
padmasaravan <padma.saravan@gmail.com>
2017-03-05 02:43:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -191,7 +191,7 @@ app.get('/articles/:articleName', function(req,res){
            res.status(500).send(err.toString());
        }else{
            if(result.rows.length === 0){
-               res.status(404).send("Data not found");
+               res.status(404).send("Article not found");
            }else{
                 var articleData = result.rows[0];
                 res.send( createTemplate(articleData) );
