01f989c1871a46784cd79225114ef7737ac83438
padmasaravan <padma.saravan@gmail.com>
2017-03-05 19:50:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/post-cmnt', function (req,res) {
 
 // Get the Article page contents from the Database and create a HTML Template
 app.get('/articles/:articleName', function(req,res){
-    pool.query("SELECT * FROM ARTICLE WHERE TITLE='"+ req.params.articleName+"'", function(err, result){
+    pool.query("SELECT * FROM ARTICLE WHERE TITLE=$1",[req.params.articleName], function(err, result){
        if (err) { // If any error
            res.status(500).send(err.toString());
        }else{
