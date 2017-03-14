1ff41497d7a5340517badb51f9679ed6c491d571
padmasaravan <padma.saravan@gmail.com>
2017-03-14 13:32:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.post('/login', function(req, res){
     
     pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err, result){
         if (err){
-            res.send(500).send(err);
+            res.status(500).send(err.toString());
         }
     });
 });
