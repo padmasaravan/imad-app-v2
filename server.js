dc27a8e14cc9df6544d578da885793e3b2194b7d
padmasaravan <padma.saravan@gmail.com>
2017-04-05 23:25:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -245,6 +245,18 @@ app.post('/submit-comment/:articleName', function (req, res) {
     }
 });
 
+app.get('/get-comments/:articleName', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT comment.*, "user".username FROM article, comment, "user" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
+});
+
 //Binding express app to port 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
