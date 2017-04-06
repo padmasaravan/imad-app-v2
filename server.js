e77fbbb4ad0a00bfb3707808e6607440c9044829
padmasaravan <padma.saravan@gmail.com>
2017-04-06 20:02:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -186,7 +186,9 @@ app.get('/check-login', function(req, res){
 
 app.get('/logout', function(req, res){
     delete req.session.auth;
-    res.send('Logout Successful');
+    //res.send('Logout Successful');
+    res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
+    
 });
 
 //Code to capture the name sent as a part of the URL
