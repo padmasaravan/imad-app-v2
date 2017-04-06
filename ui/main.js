f341adb93ff787a0dad12a005ba538966464b820
padmasaravan <padma.saravan@gmail.com>
2017-04-06 19:42:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -57,14 +57,14 @@ function loadLoginForm () {
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
-                  submit.value = 'Sucess!';
+                  submit.value = 'Success!';
               } else if (request.status === 403) {
                   submit.value = 'Invalid credentials. Try again?';
               } else if (request.status === 500) {
-                  alert('Something went wrong on the server');
+                  alert('Something went wrong on the server...');
                   submit.value = 'Login';
               } else {
-                  alert('Something went wrong on the server');
+                  alert('Something went wrong on the server...');
                   submit.value = 'Login';
               }
               loadLogin();
