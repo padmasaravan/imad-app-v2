bf2731a028e56f175a5906ef66edd186f2d9d0f2
padmasaravan <padma.saravan@gmail.com>
2017-04-06 20:19:26 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -87,7 +87,7 @@ function loadComments () {
                         <p>${escapeHTML(commentsData[i].comment)}</p>
                         <div class="commenter">
                             ${commentsData[i].username} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()} 
-                            <hr />
+                            <hrwidth="500px"/>
                         </div>
                     </div>`;
                 }
