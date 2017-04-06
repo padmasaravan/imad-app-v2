288633bf02f4276a98d4bf58d9744f9e460d7591
padmasaravan <padma.saravan@gmail.com>
2017-04-06 19:58:16 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -59,10 +59,17 @@ function loadLogin () {
 
 function escapeHTML (text)
 {
+    /*
     var $text = document.createTextNode(text);
     var $div = document.createElement('div');
     $div.appendChild($text);
     return $div.innerHTML;
+    */
+    var t = document.createTextNode(text);
+    var d = document.createElement('div');
+    d.appendChild(t);
+    return d.innerHTML;
+    
 }
 
 function loadComments () {
